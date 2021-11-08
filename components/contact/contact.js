1379d33a850a4b55881091f08a2d9e9b5bd2202d
Nicasio Ojeda Bianchi <NicaOjeda@outlook.com>
2021-11-08 12:12:28 -0300

    contact envia formulario correctamente

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -7,21 +7,36 @@
         <div class="form-contact__content">
             <div class="form-contact__name">
                 <label class="label-form" for="name">Nombre</label>
-                <input class="input-form" id="name"type="text">
+                <input class="input-form" id="name" name="name" type="text">
             </div>
             <div class="form-contact__email">
                 <label class="label-form" for="email">Email</label>
-                <input class="input-form" id="email" type="text">
+                <input class="input-form" id="email" name="to" type="text">
             </div>
             <div class="form-contact__message">
                  <label class="label-form" for="message">Mensaje</label>
-                 <textarea class="textarea-form" id="message"></textarea>
+                 <textarea class="textarea-form" name="message" id="message"></textarea>
             </div>
-            <button class="form-contact__send-form">Enviar</button>
+            <button type="submit" class="form-contact__send-form">Enviar</button>
         </div>
     </form>
     </div>`;
     const body = document.body;
     const footer = document.querySelector(".footer");
     body.insertBefore(contact, footer)
+    sendForm();
 })();
+
+function sendForm(){
+    const form = document.querySelector(".form-contact");
+    form.addEventListener("submit", (ev) =>{
+        ev.preventDefault();
+        const data = new FormData(ev.target)
+        const value = Object.fromEntries(data.entries());
+        fetch("https://apx-api.vercel.app/api/utils/dwf",{
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(value),
+        })
+    })
+}
